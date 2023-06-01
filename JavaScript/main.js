dc3b4d5367083540caede2fb655bc3309d1a5594
golden management <a.trpischovsky@hotmail.com>
2023-06-01 14:09:42 -0300

    creé Nav Bar interactiva en todas las pages

---
+++ JavaScript/main.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+var navbarMenu = document.getElementById("navbarMenu");
+if (navbarMenu.style.display === "block") {
+    navbarMenu.style.display = "none";
+} else {
+    navbarMenu.style.display = "block";
+}
+}
